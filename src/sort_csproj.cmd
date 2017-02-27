0192fd1d43420001c4d8e7e04aff7ec75d4fc346
gauffininteractive <info@onetrueerror.com>
2017-02-27 18:35:10 +0100

    sorted csproj files to make future merges easier.

---
+++ src/sort_csproj.cmd
@@ -0,0 +1,5 @@
+tools\CsProjArrange.exe --input=Server\OneTrueError.Web\OneTrueError.Web.csproj -o=Server\OneTrueError.Web\OneTrueError.Web.csproj
+tools\CsProjArrange.exe --input=Server\OneTrueError.Api\OneTrueError.Api.csproj -o=Server\OneTrueError.Api\OneTrueError.Api.csproj
+tools\CsProjArrange.exe --input=Server\OneTrueError.App\OneTrueError.App.csproj -o=Server\OneTrueError.App\OneTrueError.App.csproj
+tools\CsProjArrange.exe --input=Server\OneTrueError.SqlServer\OneTrueError.SqlServer.csproj -o=Server\OneTrueError.SqlServer\OneTrueError.SqlServer.csproj
+tools\CsProjArrange.exe --input=Server\OneTrueError.Data.Common\OneTrueError.Infrastructure.csproj -o=Server\OneTrueError.Data.Common\OneTrueError.Infrastructure.csproj
