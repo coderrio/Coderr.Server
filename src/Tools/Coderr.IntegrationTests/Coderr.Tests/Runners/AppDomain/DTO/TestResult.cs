e318fd4f2617a4dfdc0325d8f041aca126269a23
jgauffin <jonas@gauffin.org>
2019-11-03 19:17:34 +0100

    Removed ignore on TestResult

---
+++ src/Tools/Coderr.IntegrationTests/Coderr.Tests/Runners/AppDomain/DTO/TestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Coderr.Tests.Runners.AppDomain.DTO
+{
+    [Serializable]
+    public class TestResult
+    {
+        public TestResult(TestCaseToRun tc)
+        {
+            TestCase = tc;
+        }
+
+        public TimeSpan Duration { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorStackTrace { get; set; }
+
+        public TestOutcome Result { get; set; }
+        public TestCaseToRun TestCase { get; private set; }
+    }
+}
